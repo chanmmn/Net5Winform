08bc9e7cd9e3a00b77292bb507f0dc344b295c50
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-11-28 05:01:40 -0800

    .NET 5 Winform commit
    .NET 5 Winform

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtF = new System.Windows.Forms.TextBox();
+            this.txtr = new System.Windows.Forms.TextBox();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(58, 12);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(100, 23);
+            this.txtP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "P";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnCalc.Location = new System.Drawing.Point(12, 169);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "F";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "r";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "n";
+            // 
+            // textBox1
+            // 
+            this.txtF.Location = new System.Drawing.Point(58, 48);
+            this.txtF.Name = "textBox1";
+            this.txtF.Size = new System.Drawing.Size(100, 23);
+            this.txtF.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.txtr.Location = new System.Drawing.Point(58, 87);
+            this.txtr.Name = "textBox2";
+            this.txtr.Size = new System.Drawing.Size(100, 23);
+            this.txtr.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.txtn.Location = new System.Drawing.Point(58, 122);
+            this.txtn.Name = "textBox3";
+            this.txtn.Size = new System.Drawing.Size(100, 23);
+            this.txtn.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtn);
+            this.Controls.Add(this.txtr);
+            this.Controls.Add(this.txtF);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtF;
+        private System.Windows.Forms.TextBox txtr;
+        private System.Windows.Forms.TextBox txtn;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            
+            double P = 0f;
+            double F = 0f;
+            double r = 0f;
+            double n = 0f;
+
+            F = double.Parse(txtF.Text);
+            r = double.Parse(txtr.Text);
+            n = double.Parse(txtn.Text);
+
+            P = F * (r / (Math.Pow(1 + r, n) - 1)) * (1 / (1 + r));
+            txtP.Text = P.ToString();
+        }
+    }
+}
